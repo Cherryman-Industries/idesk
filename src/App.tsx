dcdb03cd315612f8a2141bb923ea6e8d7cf5bbad
ryasan86 <ryansantos86@gmail.com>
2021-03-24 12:27:03 -0700

    progress: Mobile portrait view

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,17 @@ const App = () => (
     <div className="main">
       <div className="landing">
         <div className="landing__text">
-          <h1 className="landing__title" data-aos="fade-down" data-aos-easing="ease-out">
+          <h1
+            className="landing__title"
+            data-aos="fade-down"
+            data-aos-delay="150"
+            data-aos-easing="ease-out">
             an instock solution coming soon
           </h1>
           <div
             className="landing__subtitle"
             data-aos="fade-up"
-            data-aos-delay="150"
+            data-aos-delay="450"
             data-aos-easing="ease-out">
             new website is under construction
           </div>
