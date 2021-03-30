8e647ca61d4956ee1b6a18c3bd1bc779826a82c0
ryasan86 <ryansantos86@gmail.com>
2021-03-30 15:56:00 -0700

    progress: Desktop view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
 import './scss/App.scss';
 import DownArrow from './assets/svgs/down-arrow.svg';
 
+const LandingForTablet = () => (
+  <div className="landing landing--tablet">
+    <div className="landing__content">
+      <h1
+        className="landing__title"
+        data-aos="fade-down"
+        data-aos-delay="150"
+        data-aos-easing="ease-out">
+        an instock <br /> solution <br /> coming soon
+      </h1>
+      <div
+        className="landing__subtitle"
+        data-aos="fade-up"
+        data-aos-delay="450"
+        data-aos-easing="ease-out">
+        new website is under construction
+      </div>
+    </div>
+  </div>
+);
+
+const LandingForDesktop = () => (
+  <div className="landing landing--desktop">
+    <div className="landing__content">
+      <h1
+        className="landing__title"
+        data-aos="fade-down"
+        data-aos-delay="150"
+        data-aos-easing="ease-out">
+        instock program
+      </h1>
+      <div className="landing__subtitle landing__subtitle--desktop">
+        <strong>D</strong> series&nbsp;&nbsp;&nbsp;by <strong>Carl Gustav Magnusson</strong>
+      </div>
+      <button className="landing__call-to-action-button">more</button>
+    </div>
+  </div>
+);
+
 const App = () => (
   <div className="app">
     <div className="navbar">
@@ -16,24 +55,8 @@ const App = () => (
       </div>
     </div>
     <div className="main">
-      <div className="landing">
-        <div className="landing__text">
-          <h1
-            className="landing__title"
-            data-aos="fade-down"
-            data-aos-delay="150"
-            data-aos-easing="ease-out">
-            an instock <br /> solution <br /> coming soon
-          </h1>
-          <div
-            className="landing__subtitle"
-            data-aos="fade-up"
-            data-aos-delay="450"
-            data-aos-easing="ease-out">
-            new website is under construction
-          </div>
-        </div>
-      </div>
+      <LandingForTablet />
+      <LandingForDesktop />
     </div>
   </div>
 );
