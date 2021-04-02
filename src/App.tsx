c2bd7420dffb2a38c085d26e1f81d2f5eaa0d44a
ryasan86 <ryansantos86@gmail.com>
2021-04-01 20:03:30 -0700

    progress: 5 breaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,86 +1,5 @@
 import './scss/App.scss';
 import DownArrow from './assets/svgs/down-arrow.svg';
-import MobileBG from './assets/images/Opus Room 1_V12_PP.jpg';
-import TabletBG from './assets/images/d room - d benching_V7-2_T.jpg';
-import DesktopBG from './assets/images/d room - d benching_V7_mini.png';
-
-const LandingForMobile = () => (
-  <div className="landing landing--mobile">
-    <img src={MobileBG} className="landing__bg" alt="mobile-bg" />
-    <div className="landing__content">
-      <div
-        className="landing__title"
-        data-aos="fade-down"
-        data-aos-delay="150"
-        data-aos-easing="ease-out">
-        <div className="landing__title-top">coming soon</div>
-        <div className="landing__title-bottom">instock solution</div>
-      </div>
-      <div
-        className="landing__subtitle"
-        data-aos="fade-up"
-        data-aos-delay="450"
-        data-aos-easing="ease-out">
-        new website is under construction
-      </div>
-    </div>
-  </div>
-);
-
-const LandingForTablet = () => (
-  <div className="landing landing--tablet">
-    <img src={TabletBG} className="landing__bg" alt="tablet-bg" />
-    <div className="landing__content">
-      <div
-        className="landing__title"
-        data-aos="fade-down"
-        data-aos-delay="150"
-        data-aos-easing="ease-out">
-        <div className="landing__title-top">coming soon</div>
-        <div className="landing__title-bottom">instock solution</div>
-      </div>
-      <div
-        className="landing__subtitle"
-        data-aos="fade-up"
-        data-aos-delay="450"
-        data-aos-easing="ease-out">
-        new website is under construction
-      </div>
-    </div>
-  </div>
-);
-
-const LandingForDesktop = () => (
-  <div className="landing landing--desktop">
-    <div className="landing__table-img-container">
-      <img src={DesktopBG} className="landing__table-img" alt="desktop-bg" />
-    </div>
-    <div className="landing__content">
-      <h1
-        className="landing__title"
-        data-aos="fade-down"
-        data-aos-delay="150"
-        data-aos-easing="ease-out">
-        instock program
-      </h1>
-      <div
-        className="landing__subtitle landing__subtitle--desktop"
-        data-aos="fade-up"
-        data-aos-delay="450"
-        data-aos-easing="ease-out">
-        <strong style={{ fontWeight: 400 }}>d</strong> series&nbsp;&nbsp;&nbsp;by{' '}
-        <strong style={{ fontWeight: 400 }}>Carl Gustav Magnusson</strong>
-      </div>
-      <button
-        className="landing__call-to-action-button"
-        data-aos="fade-right"
-        data-aos-delay="750"
-        data-aos-easing="ease-out">
-        more
-      </button>
-    </div>
-  </div>
-);
 
 const App = () => (
   <div className="app">
@@ -97,9 +16,25 @@ const App = () => (
       </div>
     </div>
     <div className="main">
-      <LandingForMobile />
-      <LandingForTablet />
-      <LandingForDesktop />
+      <div className="landing">
+        <div className="landing__content">
+          <div
+            className="landing__title"
+            data-aos="fade-down"
+            data-aos-delay="150"
+            data-aos-easing="ease-out">
+            <div className="landing__title-top">coming soon</div>
+            <div className="landing__title-bottom">instock solution</div>
+          </div>
+          <div
+            className="landing__subtitle"
+            data-aos="fade-up"
+            data-aos-delay="450"
+            data-aos-easing="ease-out">
+            new website is under construction
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
