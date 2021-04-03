480e3ee25d7a82f562bba9155091e0583bdeae94
ryasan86 <ryansantos86@gmail.com>
2021-04-03 16:24:23 -0700

    progress: use img instead of bg img

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,63 @@
-import './scss/App.scss';
-// import DownArrow from './assets/svgs/down-arrow.svg';
+import { useState } from 'react';
+import { useMedia } from 'react-media';
+import TallTableImg from './assets/images/table-tall.jpg';
+import WideTableImg from './assets/images/table-wide.jpg';
 
-const App = () => (
-  <div className="app">
-    <div className="navbar">
-      <span className="navbar__logo">idesk</span>
-      <div className="navbar__buttons-container">
-        {/* <ul className="navbar__burger-menu">
-          <li className="navbar__burger-menu-button"></li>
-          <li className="navbar__burger-menu-button"></li>
-        </ul>
-        <div className="navbar__arrow-container">
-          <img className="navbar__arrow-icon" src={DownArrow} alt="down-arrow" />
-        </div> */}
-      </div>
-    </div>
-    <div className="main">
-      <div className="landing">
-        <div className="landing__content">
+import './scss/App.scss';
+
+interface LandingSectionProps {
+  setIsLoading: (prop: boolean) => void;
+  isLoading: boolean;
+}
+
+const LandingSection = ({ setIsLoading }: LandingSectionProps) => {
+  const isLargeScreen = useMedia({ query: '(min-width: 1024px)' });
+  const bgImg = isLargeScreen ? WideTableImg : TallTableImg;
+
+  return (
+    <div className="landing" data-aos="fade-in" data-aos-duration="1000">
+      <img
+        src={bgImg}
+        alt="table"
+        className="landing__img"
+        onLoad={() => setIsLoading(false)}
+        onError={() => console.log('Could not load landing page image')}
+      />
+      <div className="landing__content">
+        <div
+          className="landing__title"
+          data-aos="fade-down"
+          data-aos-delay="1150"
+          data-aos-easing="ease-out">
+          <div className="landing__title-top">coming soon</div>
           <div
-            className="landing__title"
-            data-aos="fade-down"
-            data-aos-delay="150"
-            data-aos-easing="ease-out">
-            <div className="landing__title-top">coming soon</div>
-            <div className="landing__title-bottom">instock solution</div>
-          </div>
-          {/* <div
-            className="landing__subtitle"
+            className="landing__title-bottom"
             data-aos="fade-up"
-            data-aos-delay="450"
+            data-aos-delay="1300"
             data-aos-easing="ease-out">
-            new website is under construction
-          </div> */}
+            instock solution
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <div className="app">
+      <div className="navbar">
+        <span className="navbar__logo">idesk</span>
+        <div className="navbar__right"></div>
+      </div>
+      <div className="main">
+        {isLoading && <div className="loader"></div>}
+        <LandingSection setIsLoading={setIsLoading} isLoading={isLoading} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
